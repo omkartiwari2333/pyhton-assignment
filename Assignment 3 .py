9402c871856071b12eb27b995028f8ab352faa06
omkartiwari2333 <95579112+omkartiwari2333@users.noreply.github.com>
2022-08-28 18:16:48 +0530

    Add files via upload

---
+++ Assignment 3 .py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Why are functions advantageous to have in your programs?
+
+# ans: it is advantageous because once the function is created we don't have to write same program again and again we can just call out the function when we need it.
+
+# 2. When does the code in a function run: when its specified or when its called?
+
+# the code in function run when it is called by the user.
+
+# 3. What statement creates a function?
+
+# ans: def keyword is used to create a function.
+
+# 4. What is the difference between a function and a function call?
+
+# ans: fuction is created by the user to perform a specific task in a program and if same task is needed to be performed again then the fucntion can be called in between the program to perform that task we do no have to write same code again and again.
+
+# 5. How many global scopes are there in a Python program? How many local scopes?
+
+# ans: there is only 1 global scope inside a program and local scopes can be many as it is used in function as in variables so user can create as many it wants according to its requirement.
+
+# 6. What happens to variables in a local scope when the function call returns?
+
+# ans: The variables in a local scope is terminated when the function call returns.
+
+# 7. What is the concept of a return value? Is it possible to have a return value in an expression?
+
+# ans: The return is a indefined fucntion which is used to end task and returns the result of the task given after completing it 
+
+# 8. If a function does not have a return statement, what is the return value of a call to that function?
+
+# If a fucntion do not have return statement then the fucniton will be executed but the result will not be defined it will remain undefined 
+
+# 9. How do you make a function variable refer to the global variable?
+
+# Global keyword can be used to refer a function variable to the global variable 
+
+# 10. What is the data type of None?
+
+# ans:The data type of None is NoneType
+
+# 11.What does the sentence import areallyourpetsnamederic do?
+
+# ans: C:\Users\omkar\anaconda3\envs\mongotest\python.exe C:/Users/omkar/PycharmProjects/mongotest/test1.py
+# Traceback (most recent call last):
+#   File "C:/Users/omkar/PycharmProjects/mongotest/test1.py", line 1, in <module>
+#     import areallyourpetsnamederic
+# ModuleNotFoundError: No module named 'areallyourpetsnamederic'
+# 
+# Process finished with exit code 1
+
+# 12. If you had a bacon() feature in a spam module, what would you call it after importing spam?
+
+# ans: spam.bacon()
+
+# 13. What can you do to save a programme from crashing if it encounters an error?
+
+# ans: Exception statements can be used to save programme from crashing if it encounters an error
+
+# 14. What is the purpose of the try clause? What is the purpose of the except clause?
+
+# ans: Try clause helps to test the error in the code and except clause lets to handle the errors in the code.
